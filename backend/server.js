b33e7e4d55bd7e6b540df2941b5b1143810e914d
Tomer Avraham <avrahamtomer710@gmail.com>
2022-08-18 19:38:48 +0300

    init

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const users = [
+  { id: 1, firstName: "Jane", lastName: "Doe", age: 30 },
+  { id: 2, firstName: "John", lastName: "Doe", age: 35 },
+  { id: 3, firstName: "Johnny", lastName: "Doe", age: 50 },
+];
+
+const express = require("express");
+const cors = require("cors");
+const uuid = require("uuid");
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(express.json());
+
+app.get("/all", (req, res) => {
+  res.json(users);
+});
+
+app.get("/one/:id", (req, res) => {
+  const user = users.find((el) => el.id == req.params.id);
+  res.json(user || "User not found");
+});
+
+app.post("/create", (req, res) => {
+  const fullName = req.body.fullName.split(" ");
+  const newUser = {
+    id: uuid.v4(),
+    firstName: fullName[0],
+    lastName: fullName[1],
+    age: parseInt(req.body.age),
+  };
+  users.push(newUser);
+  res.json(users);
+});
+
+app.listen(3000, () => console.log("Running on 3000"));

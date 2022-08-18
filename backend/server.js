a40402708c38279062b4798f5c4ddc88930dad8b
Tomer Avraham <avrahamtomer710@gmail.com>
2022-08-18 19:43:31 +0300

    add user

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const users = [
   { id: 1, firstName: "Jane", lastName: "Doe", age: 30 },
   { id: 2, firstName: "John", lastName: "Doe", age: 35 },
   { id: 3, firstName: "Johnny", lastName: "Doe", age: 50 },
+  { id: 4, firstName: "Mike", lastName: "Foo", age: 18 },
 ];
 
 const express = require("express");
